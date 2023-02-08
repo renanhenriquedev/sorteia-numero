d6644cd8562ef0d4fc92d4ccdaf2253af68f9149
Renan <Renanhenrique20222@gmail.com>
2023-02-08 19:27:02 -0300

    feat two new function for intenger in local storage

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let cont = 0;
 
 
 function createTask() {
-    const catchUl = document.querySelector('div');
+    const catchUl = document.querySelector('ul');
     const task = document.createElement('li');
     const valueLi = input.value;
     task.id = cont;
@@ -28,30 +28,62 @@ function createTask() {
     const button = document.createElement('button');
     button.id = cont;
     button.type = 'button'
-    button.innerText = 'X';
+    button.innerHTML = 'X';
 
 
-    task.innerHTML = valueLi;
+    task.innerText = valueLi;
     catchUl.appendChild(task);
     task.appendChild(button);
 
     cont += 1
 }
 
-function remove () {
+function remove() {
     const allButton = document.querySelectorAll('button');
-    const divTask = document.querySelector('div');
-    const childrens = divTask.childNodes;
-        for (let i = 0; i < allButton.length; i += 1) {
-            allButton[i].addEventListener('click', () => {
+    const ulTask = document.querySelector('ul');
+    const childrens = ulTask.childNodes;
+    for (let i = 0; i < allButton.length; i += 1) {
+        allButton[i].addEventListener('click', () => {
             const remove = allButton[i].id
             const catchRemove = document.getElementById(remove);
-            divTask.removeChild(catchRemove);
+            ulTask.removeChild(catchRemove);
         })
-    } 
+    }
 }
 
+function storeTask() {
+    const array = [];
+    const catchLi = document.querySelectorAll('li');
+    for (let i = 0; i < catchLi.length; i += 1) {
+        let textLi = catchLi[i].innerText;
+        let newtexto = textLi.replaceAll('X', '');
+        array.push(newtexto);
+        localStorage.setItem('li', JSON.stringify(array));
+    }
+}
 
+function rescueLi() {
+    if (localStorage.getItem('li') !== null) {
+        const rescueLi = JSON.parse(localStorage.getItem('li'));
+        for (let i = 0; i < rescueLi.length; i += 1) {
+            const catchUl = document.querySelector('ul');
+            const task = document.createElement('li');
+            task.innerText = rescueLi[i];
+            task.id = cont;
+
+            catchUl.appendChild(task);
+
+            const button = document.createElement('button');
+            button.id = cont;
+
+            button.type = 'button'
+            button.innerHTML = 'X';
+            task.appendChild(button);
+            cont += 1
+            remove();
+        }
+    }
+}
 
 
 function validatesEnter() {
@@ -60,14 +92,17 @@ function validatesEnter() {
         if (e.code === 'Enter') {
             createTask();
             remove();
+            storeTask();
         }
 
     })
 }
 
 
+
 function init() {
     validatesEnter();
+    rescueLi();
 }
 
 window.onload = init;
