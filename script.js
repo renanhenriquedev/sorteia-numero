941f3e9879d96a97ab729a47e4175b7336c1b558
Renan <Renanhenrique20222@gmail.com>
2023-02-08 15:59:40 -0300

    perf for function remove

--- script.js
+++ script.js
@@ -2,13 +2,37 @@ const body = document.querySelector('body');
 
 const input = document.createElement('input');
 
-
-
 input.type = 'text';
 
 body.appendChild(input);
 
-function validatesEnter () {
+const createDiv = document.createElement('div');
+const catchDiv = document.querySelector('div');
+
+body.appendChild(createDiv);
+
+let cont = 0;
+
+
+function createTask() {
+    const catchDiv = document.querySelector('div');
+    const task = document.createElement('p');
+    const valueP = input.value;
+    task.id = cont;
+
+    const img = document.createElement('img');
+    img.src = './imagens/remove.png';
+    img.id = cont;
+
+    task.innerHTML = valueP;
+    catchDiv.appendChild(task);
+    task.appendChild(img);
+
+    cont += 1
+}
+
+
+function validatesEnter() {
     const catchInput = document.querySelector('input');
     catchInput.addEventListener('keydown', function (e) {
         if (e.code === 'Enter') {
@@ -18,15 +42,8 @@ function validatesEnter () {
     })
 }
 
-function createTask () {
-    const task = document.createElement('p');
-    const valueP = input.value;
-    task.innerHTML = valueP;
-    body.appendChild(task)
-}
 
-
-function init () {
+function init() {
     validatesEnter();
 }
 
